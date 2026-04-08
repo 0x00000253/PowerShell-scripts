88ddc7718d4390812ad80ee9619136a8f810cbda
0x00000253 <140814032+0x00000253@users.noreply.github.com>
2026-04-08 09:52:22 +0200

    Initial version script

---
+++ Switch-DnsAliasRecords.ps1
@@ -0,0 +1,19 @@
+$dnsRecords = Get-Content C:\Path\To\File.txt
+$domainFQDN = (Get-ADDomain).DNSRoot
+$targetHost = 'WhatTheAliasShouldPointTo'
+
+foreach($d in $dnsRecords) {
+  $oldRecord = Get-DnsServerResourceRecord -ComputerName $domainFQDN -ZoneName $domainFQDN -Name $d
+  $newRecord = [CimInstance]::New($oldRecord)
+  $newRecord.RecordData.HostNameAlias = $targetHost
+  Set-DnsServerResourceRecord -ComputerName $domainFQDN -ZoneName $domainFQDN -OldInputObject $oldRecord -NewInputObject $newRecord
+  [PSCustomObject]@{
+    DNSRecord = $d
+    OldTarget = $oldRecord.RecordData.HostNameAlias
+    NewTarget = $newRecord.RecordData.HostNameAlias
+    TTL = $newRecord.TimeToLive
+  }
+
+  $oldRecord = $null
+  $newRecord = $null
+}
